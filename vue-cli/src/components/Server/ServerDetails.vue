de44a7a845a23102f96c750f6d2dc7ec47ca8263
sashashv <sashash29@mail.ru>
2017-09-19 13:49:11 +0300

    vue webpack-single

---
+++ vue-cli/src/components/Server/ServerDetails.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Server Details are currently not updated</p>
+    </div>
+</template>
+<script></script>
+<style scoped>
+    div{
+        border: 1px solid red;
+    }
+</style>
---
+++ vue-cli/src/components/Server/Servers.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="index in 5">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script></script>
+<style scoped>
+    li{
+        border: 1px solid blue;
+    }
+</style>
